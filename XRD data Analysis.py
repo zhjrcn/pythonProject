b9215c87def04ce3ff50bc671f6ef45a9340b85f
zhjr <bgzhjr@qq.com>
2022-05-16 10:57:43 +0800

    Changes

--- XRD data Analysis.py
+++ XRD data Analysis.py
@@ -37,10 +37,29 @@ def plotrsm(figname, x, y, z):
     #x = np.arange(len(y))
     #print(y)
     #print(x)
-    x, y = np.meshgrid(x, y)
-    z =
-    plt.contourf(x, y, z)
-    #plt.contour(x, y, z)
+    #print(z)
+    xo = np.float16(x)
+    yo = np.float16(y)
+    zo = np.float16(z)
+    print(xo)
+    print(yo)
+    xu = np.unique(xo)
+    yu = np.unique(yo)
+    xm, ym = np.meshgrid(xu, yu)
+    zm = []
+    for j in yu:
+        zoo = []
+        for i in xu:
+            zoo_index = np.intersect1d(np.argwhere(xo == i), np.argwhere(yo == j))
+            #print("zoo_index", zoo_index)
+            #zoo = zoo.astype(np.float)
+            zoo.append(float(zo[zoo_index[0]]))
+        zm.append(zoo)
+    #Zm = np.squeeze(Zm)
+    #print(zm_index)
+    #print(Zm)
+    plt.contourf(xm, ym, zm)
+#    plt.contour(x, y, z)
     plt.grid(False)
     plt.axis('equal')
     plt.axis('tight')
@@ -71,23 +90,24 @@ def data2rsm(filename):
     y = data[:, 1]*np.pi/180
     z = data[:, 2]
 
-    print(type(x))
+    #print(data[:, 0])
     #print(data[:, 1])
     #print(data[:, 2])
 
 
     #处理数据
     Xraylambda = 1.540598
-    R = 2*np.pi*Xraylambda
-    Qx = R*(np.cos(y) - np.cos(x - y))
-    Qy = R*(np.sin(y) + np.sin(x - y))
-    data_rsm = np.vstack((Qx, Qy, z))
-    print(data_rsm)
+    R = 2*np.pi/Xraylambda
+    Qx = R*(np.cos(x) - np.cos(y - x))
+    Qy = R*(np.sin(x) + np.sin(y - x))
+    #print(Qx)
+    #print(Qy)
+    #print(data_rsm[:, 0])
+    #print(data_rsm)
     #save the result
     #filename_rsm_out = str(filename)+"RSM_out.csv"
     #data_rsm.to_csv(filename_rsm_out)     #将处理后的数据写入到文件RSM_out.csv
-    return data_rsm
-
+    return Qx, Qy, z
 
 
 print("01. 摇摆曲线作图")
@@ -104,9 +124,14 @@ if function_select == 1:
     plotxy(figname, x, y)
 elif function_select == 2:
     filename = input("请输入文件名（本文件夹下）或文件完整目录：")
-    data = data2rsm(filename)
-    x = data[:, 0]
-    y = data[:, 1]
-    z = data[:, 2]
-    figname = input("请输入图片的保存名称：")
-    plotrsm(figname, x, y, z)
+    x, y, z = data2rsm(filename)
+    #figname = input("请输入图片的保存名称：")
+    #plotrsm(figname, x, y, z)
+    data_name_rsm = str(input("请输入RSM数据的保存名称："))
+    x = pd.DataFrame(x)
+    x.to_csv('{}-x.csv'.format(data_name_rsm))
+    y = pd.DataFrame(y)
+    y.to_csv('{}-y.csv'.format(data_name_rsm))
+    z = pd.DataFrame(z)
+    z.to_csv('{}-z.csv'.format(data_name_rsm))
+    print("RSM数据已输出完成。")
